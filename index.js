b0765db730055ff8af5f03f72fc75a7b05f9622b
mcksarge <higgins.fred.s@gmail.com>
2021-12-13 21:07:42 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 // Your code here
-const dodger = document.getElementById('dodger');
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
 
+
+
+//Moves dodger left
 function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
-    }
-  }
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
 
-
-  function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left < 360) {
-      dodger.style.left = `${left + 1}px`;
-    }
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`;
   }
+}
 
 document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") {
-        moveDodgerLeft();
-    }
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
 
-    if (e.key === "ArrowRight") {
-        moveDodgerRight();
-    }
+//Moves dodger right
+
+function moveDodgerRight() {
+  const rightNumbers = dodger.style.left.replace("px", "");
+  const right = parseInt(rightNumbers, 10);
+
+  if (right < 360) {
+    dodger.style.left = `${right + 10}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
 });
